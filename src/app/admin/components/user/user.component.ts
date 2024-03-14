69dbc24563466691de043e9475844192ca5dced3
tarek <tarek.ouachani01@gmail.com>
2024-03-14 09:29:39 +0000

    styling the message in the email

--- src/app/admin/components/user/user.component.ts
+++ src/app/admin/components/user/user.component.ts
@@ -60,7 +60,44 @@ export class UserComponent {
             {
               to: this.userForm.value.email,
               subject: 'Odyssey Travel Group',
-              text: `Dear ${this.userForm.value.name} ${this.userForm.value.familyName},<br><br>Thank you for joining us! We're excited to have you on board.<br><br>Your account details are as follows:<br><b>Username: "${this.userForm.value.username}"<br>Password: "${this.userForm.value.password}"</b><br><br>Feel free to log in and explore our platform.<br><br>Best regards,<br></b><br><br></b><br>The Admin Team at Odyssey Travel Group <br><br>PS: This is an automated email, please do not reply. <br><br> Odyssey Travel Group © 2024<br> All rights reserved.<br> 50000 Meknes, MOROCCO<br> +212 690329765`
+              // text: `Dear ${this.userForm.value.name} ${this.userForm.value.familyName},<br><br>Thank you for joining us! We're excited to have you on board.<br><br>Your account details are as follows:<br><b>Username: "${this.userForm.value.username}"<br>Password: "${this.userForm.value.password}"</b><br><br>Feel free to log in and explore our platform.<br><br>Best regards,<br></b><br><br></b><br>The Admin Team at Odyssey Travel Group <br><br>PS: This is an automated email, please do not reply. <br><br> Odyssey Travel Group © 2024<br> All rights reserved.<br> 50000 Meknes, MOROCCO<br> +212 690329765`
+              text: `
+                <div style="max-width: 600px; margin: 0 auto; padding: 20px; border: 1px solid #ccc; border-radius: 8px; font-family: Arial, sans-serif;">
+                  <div style="font-size: 1.2rem; font-weight: 600; color: var(--text-dark); cursor: pointer;">ODYSSEY<span style="color: var(--primary-color);">.</span></div>
+                  <div style="line-height: 1.6;">
+                      <p>
+                          Dear ${this.userForm.value.name} ${this.userForm.value.familyName},
+                      </p>
+                      <p>
+                          Thank you for joining us! We're excited to have you on board.
+                      </p>
+                      <p>
+                          Your account details are as follows:
+                      </p>
+                      <p style="font-weight: bold;">
+                          Username: "${this.userForm.value.username}"<br>
+                          Password: "${this.userForm.value.password}"
+                      </p>
+                      <p>
+                          Feel free to log in and explore our platform.
+                      </p>
+                      <p>
+                          Best regards,
+                          <br>
+                          The Admin Team at Odyssey Travel Group
+                      </p>
+                      <p style="font-size: 0.8rem;">
+                          PS: This is an automated email, please do not reply.
+                      </p>
+                      <p style="font-size: 0.8rem;">
+                          Odyssey Travel Group © 2024<br>
+                          All rights reserved.<br>
+                          50000 Meknes, MOROCCO<br>
+                          +212 690329765
+                      </p>
+                  </div>
+                </div>
+              `
             }
           ).subscribe(
             (response) => {
