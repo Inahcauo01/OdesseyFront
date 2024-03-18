fe0c41cea9fab6dac66479e6b04111401b60dcb9
tarek <tarek.ouachani01@gmail.com>
2024-03-18 01:04:37 +0000

    style the chart 1

--- src/app/admin/components/overview/overview.component.ts
+++ src/app/admin/components/overview/overview.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import {PrimeNGConfig} from "primeng/api";
+import {StatisticService} from "../../../core/services/statistic/statistic.service";
 
 @Component({
   selector: 'app-overview',
@@ -6,62 +8,69 @@ import { Component } from '@angular/core';
   styleUrl: './overview.component.css'
 })
 export class OverviewComponent {
-  basicData: any;
   basicOptions: any;
-  lineData: any;
-  lineOptions: any;
+  numberOfUsers?: number;
+  numberOfTrips?: number;
+  numberOfReservations?: number;
+  numberOfAccommodations?: number;
+  tripData: any;
+  data: any;
 
-  constructor() {
-  }
+
+  constructor(private primengConfig: PrimeNGConfig,
+              private statisticService: StatisticService) {}
 
   ngOnInit() {
-    const documentStyle = getComputedStyle(document.documentElement);
-    const textColor = documentStyle.getPropertyValue('--text-color');
-    const textColorSecondary = documentStyle.getPropertyValue('--text-color-secondary');
-    const surfaceBorder = documentStyle.getPropertyValue('--surface-border');
+    this.getStatistic();
 
-    this.basicData = {
-      labels: ['Q1', 'Q2', 'Q3', 'Q4'],
+
+    this.primengConfig.ripple = true; // Optional, adds ripple effect to elements
+    this.statisticService.getStatistic().subscribe((result: any) => {
+      const topAccommodations = result.result.topAccommodations; // slice(0, 5) Get top 5 accommodations
+      console.log(topAccommodations);
+      // Format data for pie chart
+      this.data = {
+        labels: topAccommodations.map((accommodation: any) => accommodation.name),
+        datasets: [
+          {
+            data: topAccommodations.map((accommodation: any) => accommodation.tripCount),
+            backgroundColor: ['#FF6384', '#36A2EB', '#FFCE56', '#4BC0C0', '#9966FF'],
+            hoverBackgroundColor: ['#FF6384', '#36A2EB', '#FFCE56', '#4BC0C0', '#9966FF'],
+          }
+        ]
+      };
+    });
+
+
+    // Trip Data (Mock Data, replace it with your actual trip data)
+    this.tripData = {
+      labels: ['Trip 1', 'Trip 2', 'Trip 3', 'Trip 4', 'Trip 5', 'Trip 6'],
       datasets: [
         {
-          label: 'Sales',
-          data: [540, 325, 702, 620],
-          backgroundColor: ['rgba(255, 159, 64, 0.2)', 'rgba(75, 192, 192, 0.2)', 'rgba(54, 162, 235, 0.2)', 'rgba(153, 102, 255, 0.2)'],
-          borderColor: ['rgb(255, 159, 64)', 'rgb(75, 192, 192)', 'rgb(54, 162, 235)', 'rgb(153, 102, 255)'],
+          label: 'Number of Places',
+          data: [50, 30, 40, 60, 35, 55],
+          backgroundColor: 'rgba(255, 159, 64, 0.2)',
+          borderColor: 'rgb(255, 159, 64)',
+          borderWidth: 1
+        },
+        {
+          label: 'Reserved Places',
+          data: [40, 29, 30, 52, 30, 55],
+          backgroundColor: 'rgba(75, 192, 192, 0.2)',
+          borderColor: 'rgb(75, 192, 192)',
           borderWidth: 1
         }
       ]
     };
+  }
 
-    this.basicOptions = {
-      plugins: {
-        legend: {
-          labels: {
-            color: textColor
-          }
-        }
-      },
-      scales: {
-        y: {
-          beginAtZero: true,
-          ticks: {
-            color: textColorSecondary
-          },
-          grid: {
-            color: surfaceBorder,
-            drawBorder: false
-          }
-        },
-        x: {
-          ticks: {
-            color: textColorSecondary
-          },
-          grid: {
-            color: surfaceBorder,
-            drawBorder: false
-          }
-        }
-      }
-    };
+  private getStatistic() {
+    this.statisticService.getStatistic().subscribe((result: any) => {
+      console.log(result.result);
+      this.numberOfUsers = result.result.totalUsers;
+      this.numberOfTrips = result.result.totalTrips;
+      // this.numberOfReservations = result.result.totalReservations;
+      this.numberOfAccommodations = result.result.totalAccommodations;
+    });
   }
 }
